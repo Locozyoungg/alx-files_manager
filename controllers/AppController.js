d91f8e1fd8cd721ea36ede26da1e10968f8f18ed
Locozyoungg <collaustine27@gmail.com>
2024-09-04 23:31:34 +0300

    fixed-appcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,19 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
 
-export default class AppController {
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
   static getStatus(req, res) {
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
+    const redisLive = redisClient.isAlive();
+    const dbLive = dbClient.isAlive();
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
   static async getStats(req, res) {
-    try {
-      const [usersCount, filesCount] = await Promise.all([
-        dbClient.nbUsers(),
-        dbClient.nbFiles()
-      ]);
-
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      res.status(500).json({ error: 'Unable to retrieve statistics' });
-    }
+    const usersTotal = await dbClient.nbUsers();
+    const filesTotal = await dbClient.nbFiles();
+    res.status(200).json({ users: usersTotal, files: filesTotal });
   }
 }
+
+export default AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
-
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-module.exports = router;
